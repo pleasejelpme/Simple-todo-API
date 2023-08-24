6d6ebfd78dbce9356cb161bd75fd494f79e052e8
martin <martinvillablanca.castro@gmail.com>
2023-08-24 09:14:50 -0400

    files relocation

--- backend/todoAPI/settings.py
+++ backend/todoAPI/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = 'http://localhost:8000/api/login/'
 
